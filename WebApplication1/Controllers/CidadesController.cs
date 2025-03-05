85c3205b90edc86e65a93c8980215b5b3a8a50d4
k0yall <lukkasilva32@gmail.com>
2025-03-05 12:54:55 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/CidadesController.cs
@@ -0,0 +1,57 @@
+﻿using Cidades.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CidadesController.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CidadesController : ControllerBase
+    {
+        // Lista estática para armazenar as cidades
+        private static List<Cidade> Lista = new List<Cidade>();
+
+        // GET: api/Cidades
+        [HttpGet]
+        public List<Cidade> Get()
+        {
+            return Lista;
+        }
+
+        // POST: api/Cidades
+        [HttpPost]
+        public string Post([FromBody] Cidade cidade)
+        {
+            Lista.Add(cidade);
+            return "Cidade cadastrada com sucesso.";
+        }
+
+        // PUT: api/Cidades
+        [HttpPut]
+        public string Put([FromBody] Cidade cidade)
+        {
+            var cidadeExistente = Lista.FirstOrDefault(c => c.Codigo == cidade.Codigo);
+            if (cidadeExistente != null)
+            {
+                cidadeExistente.Nome = cidade.Nome;
+                cidadeExistente.Sigla = cidade.Sigla;
+                return "Cidade atualizada com sucesso.";
+            }
+            return "Cidade não encontrada.";
+        }
+
+        // DELETE: api/Cidades/{codigo}
+        [HttpDelete("{codigo}")]
+        public string Delete(int codigo)
+        {
+            var cidadeExistente = Lista.FirstOrDefault(c => c.Codigo == codigo);
+            if (cidadeExistente != null)
+            {
+                Lista.Remove(cidadeExistente);
+                return "Cidade excluída com sucesso.";
+            }
+            return "Cidade não encontrada.";
+        }
+    }
+}
---
+++ WebApplication1/Models/Cidades.cs
@@ -0,0 +1,14 @@
+﻿namespace Cidades.Model
+{
+    public class Cidade
+    {
+        public int Codigo { get; set; }
+        public string Sigla { get; set; }
+
+        public string Nome { get; set; }
+
+
+
+
+    }
+}
